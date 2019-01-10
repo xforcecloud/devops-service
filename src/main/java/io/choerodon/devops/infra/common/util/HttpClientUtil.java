bd0652bfd4a56e5aa9027d295144f7b87ef63b3c
youyifan <youyifan@xforceplus.com>
2019-01-10 20:59:41 +0800

    chart通过url上传到chartmuseum

--- src/main/java/io/choerodon/devops/infra/common/util/HttpClientUtil.java
+++ src/main/java/io/choerodon/devops/infra/common/util/HttpClientUtil.java
@@ -1,15 +1,21 @@
 package io.choerodon.devops.infra.common.util;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
 import io.choerodon.core.exception.CommonException;
+import org.apache.http.util.EntityUtils;
 
 public class HttpClientUtil {
 
@@ -46,6 +52,31 @@ public class HttpClientUtil {
             HttpGet httpGet = new HttpGet(sonarUrl);
             CloseableHttpResponse response1 = httpclient.execute(httpGet);
             return response1.getStatusLine().getStatusCode();
+        } catch (IOException e) {
+            throw new CommonException(e.getMessage());
+        }
+    }
+
+    /**
+     * 上传 tgz
+     *
+     * @param postUrl  tgz路径
+     * @param fileUrl 目标路径
+     */
+    public static void postTgz(String postUrl, String fileUrl) {
+        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            HttpPost httpPost = new HttpPost(postUrl);
+            FileBody bin = new FileBody(new File(fileUrl));
+            HttpEntity reqEntity = MultipartEntityBuilder.create().addPart(bin.getFile().getName(), bin).build();
+            httpPost.setEntity(reqEntity);
+
+            CloseableHttpResponse response = httpclient.execute(httpPost);
+
+            HttpEntity resEntity = response.getEntity();
+            if (resEntity != null) {
+                System.out.println("Response content length: " + resEntity.getContentLength());
+            }
+            EntityUtils.consume(resEntity);
         } catch (IOException e) {
             throw new CommonException(e.getMessage());
         }
