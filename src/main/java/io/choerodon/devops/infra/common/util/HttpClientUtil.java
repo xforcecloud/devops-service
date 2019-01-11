16c8e35b6a98599e3ebd5b57e602d846b15ba602
youyifan <youyifan@xforceplus.com>
2019-01-11 14:31:01 +0800

    chart通过url上传到chartmuseum

--- src/main/java/io/choerodon/devops/infra/common/util/HttpClientUtil.java
+++ src/main/java/io/choerodon/devops/infra/common/util/HttpClientUtil.java
@@ -1,19 +1,22 @@
 package io.choerodon.devops.infra.common.util;
 
-import io.choerodon.core.exception.CommonException;
+import java.io.*;
+
 import org.apache.http.HttpEntity;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.entity.ContentType;
+import org.apache.http.entity.InputStreamEntity;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import io.choerodon.core.exception.CommonException;
+import org.apache.http.util.EntityUtils;
 
 public class HttpClientUtil {
 
@@ -70,7 +73,8 @@ public class HttpClientUtil {
             CloseableHttpResponse response = httpclient.execute(httpPost);
             HttpEntity resEntity = response.getEntity();
             if (resEntity != null) {
-                System.out.println("Chart upload response content length: " + resEntity.getContentLength());
+                System.out.println(String.format("postUrl: s%, fileUrl: s%", postUrl, fileUrl));
+                System.out.println(String.format("Chart upload Http Status: s%, Response content length: s%", response.getStatusLine().toString(), resEntity.getContentLength()));
             }
             EntityUtils.consume(resEntity);
         } catch (IOException e) {
@@ -78,4 +82,10 @@ public class HttpClientUtil {
         }
     }
 
+    public static void main(String[] args){
+        HttpClientUtil.postTgz(
+                "http://charts.xcloud.xforceplus.com/operation-test/xmiddleware/api/charts",
+                "D:\\agile-service-0.12.1.tgz"
+        );
+    }
 }
