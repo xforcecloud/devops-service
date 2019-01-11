9eba6728167be8f1eaccaa565f472d83c6769c69
youyifan <youyifan@xforceplus.com>
2019-01-11 14:09:23 +0800

    chart通过url上传到chartmuseum

--- src/main/java/io/choerodon/devops/infra/common/util/FileUtil.java
+++ src/main/java/io/choerodon/devops/infra/common/util/FileUtil.java
@@ -1390,4 +1390,34 @@ public class FileUtil {
         return sshkeys;
     }
 
+    public static byte[] readBytesFromFile(String filePath) {
+
+        FileInputStream fileInputStream = null;
+        byte[] bytesArray = null;
+
+        try {
+
+            File file = new File(filePath);
+            bytesArray = new byte[(int) file.length()];
+
+            //read file into bytes[]
+            fileInputStream = new FileInputStream(file);
+            fileInputStream.read(bytesArray);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fileInputStream != null) {
+                try {
+                    fileInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+
+        return bytesArray;
+
+    }
 }
--- src/main/java/io/choerodon/devops/infra/common/util/HttpClientUtil.java
+++ src/main/java/io/choerodon/devops/infra/common/util/HttpClientUtil.java
@@ -1,21 +1,19 @@
 package io.choerodon.devops.infra.common.util;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-
+import io.choerodon.core.exception.CommonException;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.mime.MultipartEntityBuilder;
-import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-
-import io.choerodon.core.exception.CommonException;
 import org.apache.http.util.EntityUtils;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class HttpClientUtil {
 
@@ -66,15 +64,13 @@ public class HttpClientUtil {
     public static void postTgz(String postUrl, String fileUrl) {
         try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
             HttpPost httpPost = new HttpPost(postUrl);
-            FileBody bin = new FileBody(new File(fileUrl));
-            HttpEntity reqEntity = MultipartEntityBuilder.create().addPart(bin.getFile().getName(), bin).build();
+            ByteArrayEntity reqEntity = new ByteArrayEntity(FileUtil.readBytesFromFile(fileUrl), ContentType.APPLICATION_OCTET_STREAM);
             httpPost.setEntity(reqEntity);
 
             CloseableHttpResponse response = httpclient.execute(httpPost);
-
             HttpEntity resEntity = response.getEntity();
             if (resEntity != null) {
-                System.out.println("Response content length: " + resEntity.getContentLength());
+                System.out.println("Chart upload response content length: " + resEntity.getContentLength());
             }
             EntityUtils.consume(resEntity);
         } catch (IOException e) {
