24e16f62bb21a2c925b58e70fd322c643bca926e
gaohuikai <gaohuikai@xforceplus.com>
2019-04-18 10:22:43 +0800

    查询单个环境返回集群id

--- src/main/java/io/choerodon/devops/domain/application/entity/DevopsEnvironmentE.java
+++ src/main/java/io/choerodon/devops/domain/application/entity/DevopsEnvironmentE.java
@@ -29,6 +29,7 @@ public class DevopsEnvironmentE {
     private Boolean isConnected;
     private Boolean isActive;
     private Boolean permission;
+    private Long clusterId;
 
     private Long devopsSyncCommit;
     private Long agentSyncCommit;
@@ -44,7 +45,7 @@ public class DevopsEnvironmentE {
      * @param isActive    环境是否启用
      * @param code        环境code
      */
-    public DevopsEnvironmentE(Long id, String name, String description, Boolean isConnect, Boolean isActive, String code) {
+    public DevopsEnvironmentE(Long id, String name, String description, Boolean isConnect, Boolean isActive, String code, Long clusterId) {
 
         this.id = id;
         this.name = name;
@@ -52,6 +53,7 @@ public class DevopsEnvironmentE {
         this.isConnected = isConnect;
         this.isActive = isActive;
         this.code = code;
+        this.clusterId = clusterId;
     }
 
     /**
@@ -61,10 +63,11 @@ public class DevopsEnvironmentE {
      * @param code 环境code
      * @param name 环境name
      */
-    public DevopsEnvironmentE(Long id, String code, String name) {
+    public DevopsEnvironmentE(Long id, String code, String name, Long clusterId) {
         this.id = id;
         this.code = code;
         this.name = name;
+        this.clusterId = clusterId;
     }
 
 
@@ -107,6 +110,13 @@ public class DevopsEnvironmentE {
         this.token = token;
     }
 
+    public Long getClusterId() {
+        return clusterId;
+    }
+
+    public void setClusterId(Long clusterId) {
+        this.clusterId = clusterId;
+    }
 
     public String getDescription() {
         return description;
