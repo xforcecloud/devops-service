476372fba9d4698c51b0b5dea7c6dfee06b8f878
mumutu <luye66@163.com>
2019-05-24 10:31:52 +0800

    fix

--- src/main/java/io/choerodon/devops/infra/mapper/DevopsEnvQuotaMapper.java
+++ src/main/java/io/choerodon/devops/infra/mapper/DevopsEnvQuotaMapper.java
@@ -2,8 +2,11 @@ package io.choerodon.devops.infra.mapper;
 
 import io.choerodon.devops.infra.dataobject.DevopsEnvQuotaDO;
 import io.choerodon.mybatis.common.BaseMapper;
+import org.apache.ibatis.annotations.Param;
 
 public interface DevopsEnvQuotaMapper extends BaseMapper<DevopsEnvQuotaDO> {
 
-    DevopsEnvQuotaDO selectBy(String name, Long clusterId, Long envId);
+    DevopsEnvQuotaDO selectBy(@Param("name") String name
+            , @Param("clusterId") Long clusterId
+            , @Param("envId") Long envId);
 }
